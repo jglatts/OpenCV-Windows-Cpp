759227fc7f0e682ab3595a9d447709c314a9bc41
John Glatts <john@z-axiscc.com>
2021-12-20 17:28:55 -0500

    clean up and improvements

--- OpenCV-Test/OpenCV-Test/ShapeDetect.cpp
+++ OpenCV-Test/OpenCV-Test/ShapeDetect.cpp
@@ -3,7 +3,16 @@
 	Test with other part images
 
 	Figure out best WIRE_CONSTANT to use 
-	Smooth canny() to get crisper edges
+	Smooth canny() to get crisp edges
+
+	Incorporate Serial and File i/o
+		-> setup with a MCU to tell it where to go
+		-> in the meantime, can try and write to the file that indexer uses at 
+
+	Try other methods:
+		- find contours from src
+		- mask contours and inverse -> double checkk
+		- something like that 
 
 	Use with webcam (HiDef)!
 */
@@ -28,7 +37,7 @@ ShapeDetect::ShapeDetect() {
 	shape = ShapeDetect::ALL_SHAPES;
 }
 
-ShapeDetect::ShapeDetect(const char* img_name, bool cam_check) {
+ShapeDetect::ShapeDetect(const char* img_name) {
 	src_img = imread(img_name);
 	if (src_img.empty()) exit(1);
 	checkImgSize(src_img);
@@ -36,7 +45,7 @@ ShapeDetect::ShapeDetect(const char* img_name, bool cam_check) {
 	shape = ShapeDetect::ALL_SHAPES;
 }
 
-ShapeDetect::ShapeDetect(Mat img, bool cam_check) {
+ShapeDetect::ShapeDetect(Mat img) {
 	if (img.empty()) exit(1);
 	src_img = img.clone();
 	checkImgSize(src_img);
@@ -96,6 +105,8 @@ void ShapeDetect::detectGap() {
 }
 
 void ShapeDetect::detectGapWithCam() {
+	// implement better webcam functionality
+	// play with selecetROI()
 	Mat frame;
 	VideoCapture cap;
 	if (!cap.open(0)) {
@@ -116,17 +127,18 @@ void ShapeDetect::detectGapTest() {
 			- succesfully identifiying gap
 			- test with more cut gaps
 	*/
-	Mat images[6];
-	images[0] = imread("Part_Gap_Just_Wires.bmp");
-	images[1] = imread("Part_Gap_Wires_With_Small_Gap.bmp");
-	images[2] = imread("Part_Gap_With_Big_Gap.bmp");
-	images[3] = imread("Part_Gap_With_Big_Gap_And_Next_Group.bmp");
-	images[4] = imread("Part_Gap_After_Gap_Found.bmp");
-	images[5] = imread("Another_Gap.bmp");
-	for (int i = 0; i < 6; ++i) {
+	Mat images[] = {
+		imread("Part_Gap_Just_Wires.bmp"),
+		imread("Part_Gap_Wires_With_Small_Gap.bmp"),
+		imread("Part_Gap_With_Big_Gap.bmp"),
+		imread("Part_Gap_With_Big_Gap_And_Next_Group.bmp"),
+		imread("Part_Gap_After_Gap_Found.bmp"),
+		imread("Another_Gap.bmp")
+	};
+	for (int i = 0; i < 5; ++i) {
 		detectGap(images[i]);
 		if (i < 5) {
-			waitKey(1000);
+			waitKey(2000);
 			destroyAllWindows();
 		}
 		else waitKey();
@@ -140,7 +152,6 @@ void ShapeDetect::detectGapTest() {
 */
 void ShapeDetect::detectGap(Mat& src) {
 	Mat src_gray, src_canny, src_roi, src_thresh;
-	Mat roi_left, roi_right;
 	vector<vector<Point>> contours;
 	int wire_constant = 65;	
 	// isolate ROI and find gap
@@ -149,40 +160,48 @@ void ShapeDetect::detectGap(Mat& src) {
 	threshold(src_gray, src_thresh, 62, 255, THRESH_TOZERO_INV);
 	Canny(src_thresh, src_canny, 37, 111, 3, true);	// tweak canny + thresh to get crisper edges
 	findContours(src_canny, contours, RETR_EXTERNAL, CHAIN_APPROX_NONE);
-	cout << "# of Contours " << contours.size() << endl;
+	//cout << "# of Contours " << contours.size() << endl;
 	if (contours.size() < wire_constant) {
-		// correctly finding cut gap xD
-		//
-		// split the image into seperate ROI's
-		// find contours stored in 2 sets
-		// run convex hull on both sets of contours
-		// find distance between the convex hulls
-		// draw both sets on OG img to examine
-		// thats gap!
-		vector<vector<Point>> contours_set_one, contours_set_two;
-		vector<Point> convex_hull_points_one, convex_hull_points_two;
-		roi_left = src_canny(Range(0, src_canny.size[0]), Range(0, 47));
-		roi_right = src_canny(Range(0, src_canny.size[0]), Range(47, src_canny.size[1]));
-		findContours(roi_left, contours_set_one, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
-		findContours(roi_right, contours_set_two, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
-		convex_hull_points_one = contoursConvexHull(contours_set_one);
-		convex_hull_points_two = contoursConvexHull(contours_set_two);
-		translateContours(convex_hull_points_two, 47);
-		Rect rect_left = boundingRect(convex_hull_points_one);
-		Rect rect_right = boundingRect(convex_hull_points_two);
-		rectangle(src_roi, rect_left, Scalar(0, 0, 255), 2);
-		rectangle(src_roi, rect_right, Scalar(0, 0, 255), 2);
-		int rect_left_pos = rect_left.x + rect_left.width;
-		int rect_right_pos = rect_right.x;
-		int center_pos = (rect_left_pos + rect_right_pos) / 2;
-		line(src_roi, Point(rect_right_pos, 40), Point(rect_left_pos, 40), Scalar(0, 0, 255), 1);
-		line(src_roi, Point(center_pos, 0), Point(center_pos, src_img.size[0]), Scalar(255, 0, 0), 1);
-		imwrite("gap_found.bmp", src_roi);
+		analyzeFrame(src_roi, src_canny);
 	}
 	Mat gap_img;
 	resize(src_roi, gap_img, Size(350, 650));
 	imshow("Canny", src_canny);
-	imshow("Cut cap", gap_img);
+	imshow("Cut Gap", gap_img);
+}
+
+void ShapeDetect::analyzeFrame(Mat& src_roi, Mat& src_canny) {
+	// splits the image into 2 seperate ROI's
+	// find contours of both ROI's and stores in 2 sets
+	// run convex hull on both sets of contours
+	// find distance between the convex hulls
+	// draw both sets on OG img to examine
+	// thats gap!
+	Mat src_gray, src_thresh;
+	Mat roi_left, roi_right;
+	vector<vector<Point>> contours_set_one, contours_set_two;
+	vector<Point> convex_hull_points_one, convex_hull_points_two;
+	roi_left = src_canny(Range(0, src_canny.size[0]), Range(0, 47));
+	roi_right = src_canny(Range(0, src_canny.size[0]), Range(47, src_canny.size[1]));
+	findContours(roi_left, contours_set_one, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+	findContours(roi_right, contours_set_two, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+	convex_hull_points_one = contoursConvexHull(contours_set_one);
+	convex_hull_points_two = contoursConvexHull(contours_set_two);
+	translateContours(convex_hull_points_two, 47);
+	Rect rect_left = boundingRect(convex_hull_points_one);
+	Rect rect_right = boundingRect(convex_hull_points_two);
+	rectangle(src_roi, rect_left, Scalar(0, 0, 255), 2);
+	rectangle(src_roi, rect_right, Scalar(0, 0, 255), 2);
+	int rect_left_pos = rect_left.x + rect_left.width;
+	int rect_right_pos = rect_right.x;
+	int center_pos = (rect_left_pos + rect_right_pos) / 2;
+	cout << "The center pos is " << center_pos << endl;
+	line(src_roi, Point(rect_right_pos, 40), Point(rect_left_pos, 40), Scalar(0, 0, 255), 1);
+	line(src_roi, Point(center_pos, 0), Point(center_pos, roi_left.size[0]), Scalar(255, 0, 0), 1);
+	cout << "Distance to left-edge " << center_pos - rect_left.x << endl;
+	cout << "Distance to right-edge " << (rect_right.x + rect_right.width) - center_pos << endl;
+	//imwrite("gap_found.bmp", src_roi);
+
 }
 
 void ShapeDetect::translateContours(vector<Point>& contour_points, int scale) {
